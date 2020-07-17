ce83b7e3df8c953222b0fda4b678425216f26037
Akiva Green <green.akiva@gmail.com>
2020-07-17 15:36:07 -0400

    Added css for caluclator colors

--- p3/assets/javascript/application.js
+++ p3/assets/javascript/application.js
@@ -19,11 +19,13 @@ function calculateStatAnalysis(){
       sum += number;
   }
 
-  var mean = sum / 3.0;
+  var mean = (sum / 3.0).toFixed(2);
   var html = "<div class='row'>";
   html += "<div class='col'><h3>Max</h3><p>"+max+"</p></div>";
   html += "<div class='col'><h3>Min</h3><p>"+min+"</p></div>";
   html += "<div class='col'><h3>Range</h3><p>"+range+"</p></div>";
+  html += "</div>";
+  html += "<div class='row'>";
   html += "<div class='col'><h3>Median</h3><p>"+median+"</p></div>";
   html += "<div class='col'><h3>Mean</h3><p>"+mean+"</p></div>";
   html += "</div>";
